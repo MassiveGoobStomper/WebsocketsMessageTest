02a42cc166755ce9556f494238639410b4b709a3
MassiveGoobStomper <169108269+MassiveGoobStomper@users.noreply.github.com>
2024-05-10 22:36:17 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let socket;
-let message='';
+let incomingMessage=''
+let reader=new FileReader();
 
     function setup() {
       createCanvas(400, 200);
@@ -11,8 +12,8 @@ let message='';
       };
       socket.onmessage = (event) => {
         // Handle incoming message from the server
-        const message = JSON.parse(event.data);
-        displayMessage(message.payload);
+        incomingMessage = str(event.data[1]);
+        console.log("Recieve from Server: " + event.data);
       };
       socket.onerror = (error) => {
         console.error("WebSocket error:", error);
@@ -23,19 +24,13 @@ let message='';
     }
 
     function draw() {
-      if(message!=='')background(20);
-      else background(220)
+      background(220)
       // Draw instructions
       textAlign(CENTER);
       textSize(16);
       fill(0);
       text("Press 'A' to send message to server", width / 2, height / 2);
-      socket.onmessage = (event) => {
-        // Handle incoming message from the server
-        message = JSON.parse(event.data);
-        displayMessage(message.payload);
-      };
-      text(message, width / 2, height / 6);
+      text(incomingMessage, width / 2, height / 6);
     }
 
     function displayMessage(message) {
@@ -51,8 +46,8 @@ let message='';
       if (key === 'A' || key === 'a') {
         if (socket.readyState === WebSocket.OPEN) {
           const message = prompt("Enter message to send to server:");
-          socket.send(JSON.stringify({ type: "chat", payload: message }));
-          console.log("Message sent to server:", message);
+          socket.send(message);
+          console.log("Message sent to server:", (message));
         } else {
           console.error("WebSocket connection is not open");
         }
